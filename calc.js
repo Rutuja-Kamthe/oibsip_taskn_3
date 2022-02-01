8805e00bc86b00af9e052cfbd90de7955d533a85
Rutuja-Kamthe <90389568+Rutuja-Kamthe@users.noreply.github.com>
2022-02-01 09:16:03 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,73 @@
+/**
+    * @description      : 
+    * @author           : HP
+    * @group            : 
+    * @created          : 31/01/2022 - 14:02:48
+    * 
+    * MODIFICATION LOG
+    * - Version         : 1.0.0
+    * - Date            : 31/01/2022
+    * - Author          : HP
+    * - Modification    : 
+**/
+var a = '';
+var b = '';
+var num = [];
+var ans;
+
+// All the numbers and operators input will be stored in an array "num" using function "sendNum()"
+function sendNum(digit) {
+
+    num.push(digit);
+
+    if (num.length != 1) {
+        a = '';
+        document.getElementById('screen').innerHTML = a; // clearing the screen.
+    }
+
+
+    for (i = 0; i < num.length; i++) {
+
+        a = a + num[i]; // concatenate the elements of the array "num" into a single string, which will be displayed on the screen
+
+    }
+
+    document.getElementById('screen').innerHTML = a; // displaying the concatenated string
+
+
+}
+
+// when the user presses "=", function "equalTo()" is called 
+function equalTo() {
+    document.getElementById('screen').innerHTML = '';
+
+    for (i = 0; i < num.length; i++) {
+
+        b += num[i]; // concatenating the array "num" into a single string
+    }
+
+    ans = eval(b);
+
+    document.getElementById('screen').innerHTML = ans; // result display
+
+    while (num.length > 0) {
+        num.pop(); // emptying the array "num"
+    }
+
+    num.push(ans.toString());
+
+
+}
+
+
+// When user presses "AC", function "clearScr()" is called
+function clearScr() {
+    document.getElementById('screen').innerHTML = '';
+
+    while (num.length > 0) {
+        num.pop(); // emptying the array "num"
+    }
+
+    a = '';
+    b = '';
+}
